9f100a8dc3875fe99fda8a78036c7eb52710b175
David Stansby <dstansby@gmail.com>
2025-07-22 16:44:02 +0100

    Add option to get array in zarr (#100)
    * Add option to get array in zarr
    
    * Open array in read-only mode

--- book/data_helpers.py
+++ book/data_helpers.py
@@ -8,14 +8,16 @@ import zarr
 import zarr.storage
 
 
-def load_heart_data(array_type: Literal["numpy"]):
+def load_heart_data(array_type: Literal["numpy", "zarr"]):
     data_path = (Path(__file__).parent / "data" / "hoa_heart.zarr").resolve()
     if not data_path.exists():
         raise FileNotFoundError(f"{data_path} does not exist.")
 
-    arr_zarr = zarr.open_array(store=data_path)
+    arr_zarr = zarr.open_array(store=data_path, mode="r")
     if array_type == "numpy":
         return np.array(arr_zarr[:])
+    elif array_type == "zarr":
+        return arr_zarr
     else:
         raise ValueError(f"Did not recognise {array_type=}")
 
